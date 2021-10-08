8a79e967a7b276f9fc06a52db3e68d551d0e9e4e
Rafael Camelo <rafaelcamelo31@gmail.com>
2021-10-08 17:08:02 -0400

    add device resouce

---
+++ src/devices/devices.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { DevicesService } from './devices.service';
+import { CreateDeviceDto } from './dto/create-device.dto';
+
+@Controller('devices')
+export class DevicesController {
+  constructor(private readonly devicesService: DevicesService) {}
+
+  @Post()
+  create(@Body() createDeviceDto: CreateDeviceDto) {
+    return this.devicesService.create(createDeviceDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.devicesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.devicesService.findOne(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.devicesService.remove(+id);
+  }
+}
