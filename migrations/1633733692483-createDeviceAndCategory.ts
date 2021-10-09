c38e0520500a02d8c528ae0f5d1e7f10cc0eb8af
Rafael Camelo <rafaelcamelo31@gmail.com>
2021-10-08 20:41:47 -0400

    add database connection and migration file

---
+++ migrations/1633733692483-createDeviceAndCategory.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class createDeviceAndCategory1633733692483
+  implements MigrationInterface
+{
+  name = 'createDeviceAndCategory1633733692483';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`category\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE \`device\` (\`id\` int NOT NULL AUTO_INCREMENT, \`color\` varchar(255) NOT NULL, \`partNumner\` int NOT NULL, \`categoryId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`device\` ADD CONSTRAINT \`FK_cc4b2d26310a880c63116ed2c68\` FOREIGN KEY (\`categoryId\`) REFERENCES \`category\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`device\` DROP FOREIGN KEY \`FK_cc4b2d26310a880c63116ed2c68\``,
+    );
+    await queryRunner.query(`DROP TABLE \`device\``);
+    await queryRunner.query(`DROP TABLE \`category\``);
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CategoriesModule } from './categories/categories.module';
-import * as config from 'config';
-import { DevicesModule } from './devices/devices.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { DevicesModule } from './modules/devices/devices.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(config.get('db')),
-    DevicesModule,
-    CategoriesModule,
-  ],
+  imports: [TypeOrmModule.forRoot(), DevicesModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
